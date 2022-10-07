a7f690e1e5739f484c80b7abd7be9981bc1cf11a
Nikolay <bukharin.dev@yandex.ru>
2022-10-07 16:45:26 +0300

    BundleAnalyzer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -3,6 +3,7 @@ import webpack from "webpack";
 import { BuildOptions } from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 
 export function buildPlugins ({
   paths,
@@ -18,6 +19,9 @@ export function buildPlugins ({
     }),
     new webpack.DefinePlugin({
       __IS_DEV__: isDev
+    }),
+    new BundleAnalyzerPlugin({
+      openAnalyzer: false
     })
   ];
 
