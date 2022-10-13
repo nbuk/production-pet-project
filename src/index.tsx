3180dc93ad5de2aec2d96af1efcc25edc23e61e6
Nikolay <bukharin.dev@yandex.ru>
2022-10-13 23:33:21 +0300

    added redux store, added first entity

--- src/index.tsx
+++ src/index.tsx
@@ -3,21 +3,22 @@ import { createRoot } from "react-dom/client";
 import { App } from "app/App";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "app/providers/ThemeProvider";
-
-import "shared/config/i18n/i18n";
 import ErrorBoundary from "app/providers/ErrorBoundary/ui/ErrorBoundary";
-
+import { StoreProvider } from "app/providers/StoreProvider";
+import "shared/config/i18n/i18n";
 import "app/styles/index.scss";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-  <ErrorBoundary>
-    <BrowserRouter>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
-  </ErrorBoundary>,
+  <StoreProvider>
+    <ErrorBoundary>
+      <BrowserRouter>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </ErrorBoundary>
+  </StoreProvider>,
 );
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,10 +1,17 @@
 import { FC } from "react";
 import { useTranslation } from "react-i18next";
+import { Counter } from "entities/Counter";
 
 const MainPage: FC = () => {
   const { t } = useTranslation("main");
 
-  return <div>{t("Главная")}</div>;
+  return (
+    <>
+      <div>{t("Главная")}</div>
+      <Counter />
+    </>
+
+  );
 };
 
 export default MainPage;
